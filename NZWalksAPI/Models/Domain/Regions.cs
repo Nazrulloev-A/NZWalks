4795df43dd3bfa20ea148d8230ea6586ec954f30
Naz-Amer <smith2011@inbox.ru>
2023-10-28 13:12:09 -0500

    added domain models with the properties

---
+++ NZWalksAPI/Models/Domain/Regions.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalksAPI.Models.Domain;
+
+public class Regions
+{
+    public Guid  Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+
+    public string? RegionalmageUrl { get; set; }
+
+
+}
